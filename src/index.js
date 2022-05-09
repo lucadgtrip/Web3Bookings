06459de55577f72e13cf9c417941526789a5177e
Luca <34341206+lucadgtrip@users.noreply.github.com>
2022-05-09 09:59:23 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { NotificationProvider } from 'web3uikit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="xxxx" serverUrl="xxxx">
+    <MoralisProvider appId="LvsVD3guJJRk99xVw9xH3EzI6BFhFlyzhTQSJQRd" serverUrl="https://mrwz9isfndc0.usemoralis.com:2053/server">
       <NotificationProvider>
       <BrowserRouter>
         <App />
